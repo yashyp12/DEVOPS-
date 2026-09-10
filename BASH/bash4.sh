79f99dd027b19aba7d6ee57d3521185b13431337
Yash Patil <patilyashyp12@gmail.com>
2026-09-10 18:12:40 +0530

    Auto-commit: 2026-09-10 18:12:40

--- BASH/bash4.sh
+++ BASH/bash4.sh
@@ -4,4 +4,12 @@ echo "Hello world what you are doing today  $name "
 read -p "What is your age " age
 echo " Okay so your age is  $age"
 
+if [[  "$age" -lt 30 ]]; then
+     echo " Your age is less than 30 "
+elif [[ "$age" -gt 15 ]]; then
+     echo " Your  age is greatet than 15 "
+else 
+     echo " Please answer with correct option " 
+fi
+
 
